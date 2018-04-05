b80d17866700e9076312d5a67ea88e908285ecc2
jfong361 <jfong361@gmail.com>
2018-04-05 14:01:24 +1000

    Add spectratype distribution plotter

--- R/distributions.R
+++ R/distributions.R
@@ -1,6 +1,42 @@
+#' Histogram plotter
+#' @description  Plots histogram for all sample in dataframes.
+#' If length(sampleNames) == 1, then the bars will also have
+#' y-values (or x if horizontal plot) labels on them.
+#' Use 'perc' to control if the values are percentages.
+#'
+#' @param dataframes  list type. List of dataframes
+#' @param sampleNames vector type. 1-1 correspondence to dataframes.
+#' @param plotTitle string type.
+#' @param vert boolean type. True if the plot should be vertical
+#' @param xlabel string type
+#' @param ylabel string type
+#' @param perc boolean type. True if data's axis is a percentage
+#' proportion (instead of 0-1) only used if length(sampleNames) == 1
+#' @param subtitle string type
+#' @param sorted boolean type. True if histogram should be sorted in descending
+#' order
+#' @param cutoff int type. Number of maximum ticks to show (x on vert plots,
+#' y on hori plots).
+#'
+#' @return ggplot2 object
+#' @export
+#'
+#' @examples
 .plotDist <- function(dataframes, sampleNames, plotTitle, vert = T,
                       xlabel = "", ylabel = "", perc = T, subtitle = "",
                       sorted = T, cutoff = 15) {
+    # Args:
+    #   dataframes:
+    #   sampleNames: A
+    #   plotTitle: A
+    #   vert: A
+    #   xlabel: A
+    #   ylabel: A string type.
+    #   perc:
+    #   subs: subtitle. defaults to empty
+    #   sortDist: sorts plot by descending distribution order, True by default
+    # Returns:
+    #   ggplot().
     require(ggplot2)
     frames <- length(dataframes)
     # sanity check
@@ -162,3 +198,84 @@
              caption = caps)
     return(g)
 }
+
+
+#' Title Spectratype plotter
+#' @description Plots length distribution
+#' @param dataframes list type. List of dataframes.
+#' @param sampleNames vector type. 1-1 correspondance with dataframes
+#' @param region string type. Region that will be displayed in the plot title.
+#' This specifies which region this spectratype belongs to. If not supplied,
+#' a default (start, end) range will be displayed instead
+#' @param title string type. Ignored if region is specified.
+#' @param subtitle string type
+#' @param xlabel string type
+#' @param ylabel string type
+#'
+#' @return ggplot2 object
+#' @export
+#'
+#' @examples
+.plotSpectratype <- function(dataframes, sampleNames, region,
+                             title = "Spectratype", subtitle = "",
+                             xlabel = "Length(AA)",
+                             ylabel = "Distribution") {
+    nsample <- length(dataframes)
+    stopifnot(nsample == length(sampleNames))
+    if (nsample != length(sampleNames)) {
+        stop(paste("Expected equal number of sample names and dataframes, got",
+                   length(sampleNames), "samples and", nsample, "dataframes."))
+    }
+
+    # pre-processing
+    for (i in seq_len(nsample)) {
+        df <- dataframes[[i]]
+        df$sample <- rep(sampleNames[i], nrow(df))
+        df$percent <- df$count / sum(df$count)
+        dataframes[[i]] <- df
+    }
+
+    # merge
+    df.union <- do.call("rbind", dataframes)
+
+    # title logic
+    # if reion is not missing, it's a spectratype for FR/CDRs
+    # else, it's a 'general' spectratype plot.
+    # Can be reused for other length distributions E.G.: whole seq length
+    subtitle <- paste(subtitle, paste(unlist(lapply(dataframes, function(x) {
+        sum(x$count)
+        })), collapse = ", "), "sequences with")
+    if (!missing(region)) {
+        plotTitle <- paste(region, "amino acid spectratype")
+        plotSubTitle <- paste(subtitle ,"distribution of", region,
+                              "amino acid lengths",
+                              min(df.union$length), "to",
+                              max(df.union$length))
+    } else {
+        plotTitle <- title
+        plotSubTitle <- paste(subtitle, "lengths", min(df.union$length),
+                              "to", max(df.union$length))
+    }
+    # Always name your sample(s)!
+    plotTitle <- paste(plotTitle, "in", paste(sampleNames, collapse = ", "))
+
+    g <- ggplot(df.union, aes(length, percent))
+
+    if (nsample == 1) {
+        g <- g + geom_bar(stat = "identity", aes(fill = sample),
+                          position = "dodge",
+                          show.legend = FALSE, fill = BLUEHEX) +
+            geom_text(aes(label = count), vjust = -1, size = 3)
+    } else {
+        g <- g + geom_bar(stat = "identity",
+                          aes(fill = sample), position = "dodge")
+    }
+    g <- g + labs(title = plotTitle,
+                  subtitle = plotSubTitle, x = xlabel, y = ylabel)
+    #geom_smooth(aes(colour=round), se=F,
+    # method="glm", formula=y~ns(x, 3), lwd=0.7)+
+    #geom_text_repel(aes(label = count), size = 3) +
+    #geom_text(aes(label = count), vjust=-1, size = 3) +
+    return(g)
+}
+
--- R/util.R
+++ R/util.R
@@ -27,17 +27,23 @@ getTotal <- function(filename) {
 }
 
 listFilesInOrder <- function(path, pattern, expectedRet = c(1)) {
-    # Returns files in order of path. I.e overrides list.files' behaviour of sorted files
-    # This is crucial because we are assuming sampleNames is in 1-1 correspondance with dataframes.
-    # This can be achieved by manually iterating over all sample path in the provided vector of path
-    # and appending to a vector. Sometimes, (like abundance) list.files will return more that one
-    # matching file (gene, family, variant), so expectedRet is there to ensure that we aren't doing
-    # something silly. expectRet is a vector because sometimes there might be more than one possible configuration.
-    # EG: abundance plot may or maynot have D gene when analyzing heavy/light chains
+    # Returns files in order of path.
+    # i.e overrides list.files' behaviour of sorted files
+    # This is crucial because we are assuming sampleNames is
+    # in 1-1 correspondance with dataframes.
+    # This can be achieved by manually iterating over all sample path
+    # in the provided vector of path and appending to a vector.
+    # Sometimes, (like abundance) list.files will return more that one
+    # matching file (gene, family, variant), so expectedRet is there to ensure
+    # that we aren't doing something silly. expectRet is a vector because
+    # sometimes there might be more than one possible configuration.
+    # EG: abundance plot may or maynot have D gene when analyzing heavy/light
+    # chains
     # Returns: ordered vector of files (according to provided path's ordering)
     orderedFiles <- c()
     for (p in path) {
-        retval <- list.files(path = p, pattern = pattern, full.names = TRUE, recursive = TRUE)
+        retval <- list.files(path = p, pattern = pattern,
+                             full.names = TRUE, recursive = TRUE)
         if (length(retval) == 0) {
             return(c())
         }
